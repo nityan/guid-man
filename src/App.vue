71f370c95e3977f7b67d0f5ec3fed8581ec753f0
Nityan Khanna <nityan.khanna@gmail.com>
2023-08-29 20:11:49 -0400

    run linter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <GuidMan />
+	<GuidMan />
 </template>
 <script>
 import GuidMan from './components/GuidMan.vue'
 
 export default {
-  name: 'App',
-  components: {
-    GuidMan
-  }
+	name: 'App',
+	components: {
+		GuidMan
+	}
 }
 </script>
 
 <style>
-  @import './assets/bootstrap/css/bootstrap.min.css';
+@import './assets/bootstrap/css/bootstrap.min.css';
 </style>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- src/components/GuidMan.vue
+++ src/components/GuidMan.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-    <img alt="Need a GUID?" src="../assets/need-a-guid.png" class="img-fluid">
-    <div>
-      <button class="btn btn-primary" v-on:click="generateGuid()">Generate a GUID</button>
-    </div>
-    <span>{{ guid }}</span>
-  </div>
+	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+		<img alt="Need a GUID?" src="../assets/need-a-guid.png" class="img-fluid" />
+		<div>
+			<button class="btn btn-primary" v-on:click="generateGuid()">Generate a GUID</button>
+		</div>
+		<span>{{ guid }}</span>
+	</div>
 </template>
 
 <script>
-import { uuid } from 'vue-uuid';
+import { uuid } from 'vue-uuid'
 
 export default {
-  name: 'GuidMan',
-  data() {
-    return {
-      guid: null,
-    }
-  },
-  methods: { 
-    generateGuid() {
-      this.guid = uuid.v4().toUpperCase()
-    }
-  }
+	name: 'GuidMan',
+	data() {
+		return {
+			guid: null
+		}
+	},
+	methods: {
+		generateGuid() {
+			this.guid = uuid.v4().toUpperCase()
+		}
+	}
 }
 </script>
